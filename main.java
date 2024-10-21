6fdd3b3959f8226427c0da7d138e9ee4726cf324
Microwaved-Pie <144716146+Microwaved-Pie@users.noreply.github.com>
2024-10-21 18:08:00 +0000

    added base files.

---
+++ main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class main
+{ 
+  public static void main(String[] args) 
+  { 
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Do you want to start the game?");
+    String userInput = sc.nextLine();
+    if (userInput != y)
+      System.out.println("Hello World");
+    
+  } 
+} 
